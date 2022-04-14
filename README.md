6fd4229a5427a99be71edc3c54cb0775a0c0d435
jqiuyin <943571574@qq.com>
2022-04-14 19:56:09 +0800

    streamline

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# fk_learning
+# 自动学习
+
+## 使用方法
+
+1. windows安装微信并登录
+1. 截取微信左上角头像替换image/head.png
+1. `git clone https://github.com/jqiuyin/fk_learning.git`
+1. `cd fk_learning`
+1. `pip install -r requirements.txt`
+1. 运行`python main.py`
+
+## 运行要求
+
+1. windows
+1. python3.8
+1. PyAutoGUI==0.9.53
+
+## 注意事项
+
+1. 仅在2560x1080分辨率的屏幕上测试过
+1. 仅支持windows新系统
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pyautogui as pg
 import time
 
-answer=pg.prompt(text='请输入课程中的答案', title='答案' , default='')
-answers=answer.split(',')
-print(answers)
+answer = pg.prompt(text='请输入课程中的答案', title='答案', default='')
+answers = answer.split(',')
+
+
+def click(image_path, grayscale=True, sleep_time=3):
+    point = pg.locateCenterOnScreen(image_path, grayscale=grayscale)
+    pg.click(point.x, point.y)
+    time.sleep(sleep_time)
+    return point
 
 
 pg.hotkey('ctrl', 'w')
 head = pg.locateCenterOnScreen('image/head.png')
-
 contact = pg.Point(x=head.x, y=(head.y+95))
 pg.click(contact.x, contact.y)
 
@@ -16,88 +21,54 @@ pg.moveTo(x=(contact.x+150), y=contact.y)
 for s in range(100):
     pg.scroll(10000)
 
-official_accounts = pg.locateCenterOnScreen('image/official_accounts.png',grayscale=True)
-pg.click(official_accounts.x,official_accounts.y)
-
-qcsh = pg.locateCenterOnScreen('image/qcsh.png',grayscale=True)
-pg.click(qcsh.x,qcsh.y)
-time.sleep(3)
-
-send_message=pg.locateCenterOnScreen('image/send_message.png',grayscale=True)
-pg.click(send_message.x,send_message.y)
-time.sleep(3)
-
-qndxx=pg.locateCenterOnScreen('image/qndxx.png',grayscale=True)
-pg.click(qndxx.x,qndxx.y)
-time.sleep(10)
-
-me=pg.locateCenterOnScreen('image/me.png',grayscale=True)
-pg.click(me.x,me.y)
-time.sleep(3)
+click('image/official_accounts.png')
+click('image/qcsh.png')
+click('image/send_message.png')
+click('image/qndxx.png', sleep_time=10)
+me = click('image/me.png')
 
 pg.moveTo(x=me.x, y=me.y-100)
 for s in range(10):
     pg.scroll(-100)
 time.sleep(3)
 
-learning_task=pg.locateCenterOnScreen('image/learning_task.png',grayscale=True)
-pg.click(learning_task.x,learning_task.y)
+click('image/learning_task.png')
+
+no_more = pg.locateCenterOnScreen('image/no_more.png', grayscale=True)
+pg.click(no_more.x, no_more.y-50)
 time.sleep(3)
 
-no_more=pg.locateCenterOnScreen('image/no_more.png',grayscale=True)
-pg.click(no_more.x,no_more.y-50)
-time.sleep(3)
+click('image/tark_part_in.png', sleep_time=10)
+click('image/learning.png', sleep_time=10)
 
-tark_part_in=pg.locateCenterOnScreen('image/tark_part_in.png',grayscale=True)
-pg.click(tark_part_in.x,tark_part_in.y)
-time.sleep(10)
-
-learning=pg.locateCenterOnScreen('image/learning.png',grayscale=True)
-pg.click(learning.x,learning.y)
-time.sleep(10)
-
-welcome = pg.locateCenterOnScreen('image/welcome.png',grayscale=True)
+welcome = pg.locateCenterOnScreen('image/welcome.png', grayscale=True)
 if welcome:
-    pg.click(welcome.x,welcome.y+30)
+    pg.click(welcome.x, welcome.y+30)
     time.sleep(3)
 
-    city = pg.locateCenterOnScreen('image/city.png',grayscale=True)
-    pg.click(city.x,city.y)
-    time.sleep(3)
+    click('image/city.png')
 
-    pg.click(welcome.x,welcome.y+60)
+    pg.click(welcome.x, welcome.y+60)
 
-    area = pg.locateCenterOnScreen('image/area.png',grayscale=True)
-    pg.click(area.x,area.y)
-    time.sleep(3)
+    click('image/area.png')
+    click('image/enter.png', sleep_time=10)
 
-    enter = pg.locateCenterOnScreen('image/enter.png')
-    pg.click(enter.x,enter.y)
-    time.sleep(10)
-
-start = pg.locateCenterOnScreen('image/start.png')
-pg.click(start.x,start.y)
+click('image/start.png')
 
 while True:
-    if pg.locateCenterOnScreen('image/enter_answer.png',grayscale=True):
+    if pg.locateCenterOnScreen('image/enter_answer.png', grayscale=True):
         break
     time.sleep(60)
 
 for answer in answers:
     for i in answer:
-        print(i)
-        point=pg.locateCenterOnScreen('image/%s.png' % i,grayscale=True)
-        pg.click(point.x,point.y)
-    enter_answer=pg.locateCenterOnScreen('image/enter_answer.png',grayscale=True)
-    pg.click(enter_answer.x,enter_answer.y)
-    time.sleep(10)
+        click('image/%s.png' % i)
+    click('image/enter_answer.png', sleep_time=10)
 
-continue_learning = pg.locateCenterOnScreen('image/continue.png')
-pg.click(continue_learning.x,continue_learning.y)
-
+click('image/continue.png', sleep_time=10, grayscale=False)
 
 while True:
-    if pg.locateCenterOnScreen('image/end.png',grayscale=True):
+    if pg.locateCenterOnScreen('image/end.png', grayscale=True):
         break
     time.sleep(60)
 
